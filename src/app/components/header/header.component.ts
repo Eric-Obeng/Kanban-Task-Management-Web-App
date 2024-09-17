1a6e20de026e68abea82e6ca68e6b995f28748fd
Eric-Obeng <obengeric.3958@gmail.com>
2024-09-17 10:58:48 +0000

    updated header styles

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../../shared/services/api.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  constructor(private apiServie: ApiService) {}
 
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+
+    this.apiServie.getAllBoards().subscribe({
+      next: (val) => console.log(val),
+    });
+
+    
+  }
 }
