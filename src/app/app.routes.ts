4ba4afbf6a13df3bc549ff603126ac129b5128cb
Eric-Obeng <obengeric.3958@gmail.com>
2024-09-17 00:07:54 +0000

    updated route to show mobile menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MenuMobileComponent } from './components/menu-mobile/menu-mobile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MenuMobileComponent,
+  },
+];
