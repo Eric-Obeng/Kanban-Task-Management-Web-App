7c529db6a03ad6220eeb66fef109bfc148fb00eb
Eric-Obeng <obengeric.3958@gmail.com>
2024-09-18 06:19:51 +0000

    gets boards from store

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { IBoard } from '../../interfaces/board';
+import { selectAllBoards } from '../../shared/state/board/board.selectors';
+import * as BoardActions from '../../shared/state/board/board.actions';
+import { AsyncPipe, CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-board',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, AsyncPipe],
   templateUrl: './board.component.html',
   styleUrl: './board.component.scss',
 })
 export class BoardComponent {
+  boards$!: Observable<IBoard[]>;
   constructor(private store: Store) {}
+
+  ngOnInit(): void {
+    this.boards$ = this.store.select(selectAllBoards);
+  }
 }
