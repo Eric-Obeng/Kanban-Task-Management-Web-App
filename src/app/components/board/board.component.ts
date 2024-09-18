91d95ade8c8a6da0cc112463d377a945131c3572
Eric-Obeng <obengeric.3958@gmail.com>
2024-09-18 14:47:13 +0000

    baord component upated

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -2,22 +2,28 @@ import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { IBoard } from '../../interfaces/board';
-import { selectAllBoards } from '../../shared/state/board/board.selectors';
+import {
+  selectAllBoards,
+  selectSelectedBoard,
+} from '../../shared/state/board/board.selectors';
 import * as BoardActions from '../../shared/state/board/board.actions';
 import { AsyncPipe, CommonModule } from '@angular/common';
+import { BoardItemComponent } from './board-item/board-item.component';
 
 @Component({
   selector: 'app-board',
   standalone: true,
-  imports: [CommonModule, AsyncPipe],
+  imports: [CommonModule, AsyncPipe, BoardItemComponent],
   templateUrl: './board.component.html',
   styleUrl: './board.component.scss',
 })
 export class BoardComponent {
-  boards$!: Observable<IBoard[]>;
+  boards$!: Observable<IBoard | null | undefined>;
+
   constructor(private store: Store) {}
 
   ngOnInit(): void {
-    this.boards$ = this.store.select(selectAllBoards);
+    this.boards$ = this.store.select(selectSelectedBoard)
+    console.log(this.boards$);
   }
 }
