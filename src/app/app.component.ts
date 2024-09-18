1b9ee9ecc309ad38314e11feb7edd55cfdf9ed09
Eric-Obeng <obengeric.3958@gmail.com>
2024-09-18 06:22:02 +0000

    minor updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,8 @@ import { MenuMobileComponent } from './components/menu-mobile/menu-mobile.compon
 import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { Store } from '@ngrx/store';
 import { ApiService } from './shared/services/api.service';
-import { loadBoards } from './shared/state/board.actions';
+import { loadBoards } from './shared/state/board/board.actions';
+import { loadTheme } from './shared/state/theme/theme.actions';
 
 @Component({
   selector: 'app-root',
@@ -22,12 +23,12 @@ import { loadBoards } from './shared/state/board.actions';
 export class AppComponent {
   title = 'Kanban-Task-Managment-Web-App';
 
-  constructor(private store: Store, private apiService: ApiService) {
-    this.store.dispatch(loadBoards());
-  }
+  constructor(private store: Store, private apiService: ApiService) {}
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
+    this.store.dispatch(loadBoards());
+    this.store.dispatch(loadTheme());
   }
 }
