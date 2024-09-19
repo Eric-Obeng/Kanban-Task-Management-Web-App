610444dcc4e9bf0366a9bb526d50b9ec8defa9ac
Eric-Obeng <obengeric.3958@gmail.com>
2024-09-19 10:29:34 +0000

    form to add and create board

---
+++ src/app/components/modal/forms/board-form/board-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-board-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './board-form.component.html',
+  styleUrl: './board-form.component.scss'
+})
+export class BoardFormComponent {
+
+}
