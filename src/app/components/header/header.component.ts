53c2c6133b315a8743f452be676e25504efd99b8
Eric-Obeng <obengeric.3958@gmail.com>
2024-09-19 10:28:39 +0000

    heaser shows selected board name

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -4,30 +4,23 @@ import { MenuMobileComponent } from '../menu-mobile/menu-mobile.component';
 import { Observable } from 'rxjs';
 import { IBoard } from '../../interfaces/board';
 import { Store } from '@ngrx/store';
+import { selectSelectedBoard } from '../../shared/state/board/board.selectors';
+import { CommonModule } from '@angular/common';
 // import { selectSelectedBoard } from '../../shared/state/board/board.selectors';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MenuMobileComponent],
+  imports: [MenuMobileComponent, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-  selectedBoards$!: Observable<IBoard>
+  selectedBoards$!: Observable<IBoard | null>;
   showMenuModal: boolean = false;
 
-  constructor(private apiServie: ApiService, private store: Store) {}
-
-  ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-
-    this.apiServie.getAllBoards().subscribe({
-      next: (val) => console.log(val),
-    });
-
-    // this.selectedBoards$ = this.store.select(selectSelectedBoard)
+  constructor(private apiServie: ApiService, private store: Store) {
+    this.selectedBoards$ = this.store.select(selectSelectedBoard);
   }
 
   onShowMenuModal() {
