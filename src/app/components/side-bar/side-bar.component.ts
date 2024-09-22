276b594f40c414b7ec47236913d17f157b3ec3b4
Eric-Obeng <obengeric.3958@gmail.com>
2024-09-22 12:10:03 +0000

    updated side bar

--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -1,10 +1,13 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
 import { MenuMobileComponent } from '../menu-mobile/menu-mobile.component';
 import { ThemeComponent } from '../theme/theme.component';
 import { CommonModule } from '@angular/common';
 import { BoardService } from '../../shared/services/board.service';
 import { Store } from '@ngrx/store';
-import { setSelectedBoard } from '../../shared/state/board/board.actions';
+import {
+  selectBoard,
+  setSelectedBoard,
+} from '../../shared/state/board/board.actions';
 import { IBoard } from '../../interfaces/board';
 import {
   selectSelectedBoard,
@@ -19,20 +22,24 @@ import {
   styleUrl: './side-bar.component.scss',
 })
 export class SideBarComponent {
-  boards = this.boardService.boards$;
-  totalBoards = this.boardService.totalBoards$;
+  dataService = inject(BoardService);
+
+  boards = this.dataService.boards$;
+  totalBoards = this.dataService.totalBoards$;
   selectedBoard$ = this.store.select(selectSelectedBoard);
 
   showSidebar: boolean = true;
 
   @Output() createBoardClicked = new EventEmitter<void>();
 
-  constructor(private boardService: BoardService, private store: Store) {}
+  constructor(private store: Store) {}
 
   onBoardClick(board: IBoard) {
     this.store.dispatch(setSelectedBoard({ board }));
-    localStorage.setItem('selectedBoard', JSON.stringify(board)); // Save to local storage
+    localStorage.setItem('selectedBoard', JSON.stringify(board));
     console.log(board);
+    this.dataService.selectBoard(board);
+    console.log('Board clicked and selected:', board);
   }
 
   hideSidebar() {
