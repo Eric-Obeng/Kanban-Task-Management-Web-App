ddf9c3a6ec26003f2ddd422b1144eee9769b9c94
Eric-Obeng <obengeric.3958@gmail.com>
2024-09-23 18:35:49 +0000

    implements mark as completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,9 @@
 import { Routes } from '@angular/router';
 import { BoardComponent } from './components/board/board.component';
-import { TaskDeatilsComponent } from './components/modal/task-deatils/task-deatils.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: BoardComponent,
-  },
-  {
-    path: 'task',
-    component: TaskDeatilsComponent,
   },
 ];
--- src/app/components/modal/task-deatils/task-deatils.component.ts
+++ src/app/components/modal/task-deatils/task-deatils.component.ts
@@ -1,20 +1,93 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  SimpleChanges,
+  ChangeDetectorRef,
+  OnInit,
+  OnChanges,
+} from '@angular/core';
 import { ActionsComponent } from '../actions/actions.component';
 import { ITask } from '../../../interfaces/task';
-import { TaskFormComponent } from "../forms/task-form/task-form.component";
+import { TaskFormComponent } from '../forms/task-form/task-form.component';
+import { IBoard } from '../../../interfaces/board';
+import { BoardService } from '../../../shared/services/board.service';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { Store } from '@ngrx/store';
+import * as BoardActions from '../../../shared/state/board/board.actions';
 
 @Component({
-  selector: 'app-task-deatils',
+  selector: 'app-task-details',
   standalone: true,
-  imports: [ActionsComponent, TaskFormComponent],
+  imports: [ActionsComponent, TaskFormComponent, CommonModule],
   templateUrl: './task-deatils.component.html',
-  styleUrl: './task-deatils.component.scss',
+  styleUrls: ['./task-deatils.component.scss'],
 })
-export class TaskDeatilsComponent {
+export class TaskDetailsComponent implements OnInit, OnChanges {
   @Output() hideMenu = new EventEmitter<void>();
   @Input() task!: ITask;
+  @Output() taskChange = new EventEmitter<ITask>();
+  board$!: Observable<IBoard | null | undefined>;
+  completedSubtaskCount = 0;
+  statuses: string[] = [];
+  boardId!: string;
 
   showTaskForm = false;
+
+  constructor(
+    private boardService: BoardService,
+    private store: Store,
+    private cdr: ChangeDetectorRef
+  ) {}
+
+  ngOnInit(): void {
+    this.boardService.selectedBoard$.subscribe((board) => {
+      if (board) {
+        this.statuses = board.columns.map((column) => column.name);
+        this.boardId = board.id;
+      }
+    });
+    this.updateCompletedSubtaskCount();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['task']) {
+      this.updateCompletedSubtaskCount();
+    }
+  }
+
+  markAsCompleted(index: number, event: Event) {
+    const checkbox = event.target as HTMLInputElement;
+    const isCompleted = checkbox.checked;
+
+    const updatedTask = {
+      ...this.task,
+      subtasks: this.task.subtasks.map((subtask, i) =>
+        i === index ? { ...subtask, isCompleted } : subtask
+      ),
+    };
+
+    // Dispatch the action with the boardId
+    this.store.dispatch(
+      BoardActions.updateTask({
+        boardId: this.boardId,
+        columnName: this.task.status,
+        task: updatedTask,
+      })
+    );
+
+    this.taskChange.emit(updatedTask);
+    this.updateCompletedSubtaskCount();
+  }
+
+  private updateCompletedSubtaskCount() {
+    this.completedSubtaskCount = this.task.subtasks.filter(
+      (subtask) => subtask.isCompleted
+    ).length;
+    this.cdr.detectChanges();
+  }
 
   openForm() {
     this.showTaskForm = true;
